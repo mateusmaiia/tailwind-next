da8093b32012520b6547142766afcd1608fd73e4
Maia <matteusmaia889@gmail.com>
2024-06-11 17:19:12 -0300

    feat: MainNavigation

--- src/Components/MainNavigation/index.tsx
+++ src/Components/MainNavigation/index.tsx
@@ -1,18 +1,22 @@
-import { ChevronDown, Home } from 'lucide-react'
+import {
+  BarChart,
+  CheckSquare,
+  Flag,
+  HomeIcon,
+  SquareStack,
+  Users,
+} from 'lucide-react'
+import { NavItem } from './NavItem'
 
 export function MainNavigationq() {
   return (
     <nav className="space-y-0.5">
-      <a
-        href=""
-        className=" group flex rounded px-3 py-2 hover:violet-50 items-center gap-3"
-      >
-        <Home className="h-5 w-5 text-zinc-500" />
-        <span className="font-medium text-zinc-700 group-hover:text-violet-500 ">
-          Home
-        </span>
-        <ChevronDown className="h-5 w-5 text-zinc-400 group-hover:text-violet-300" />
-      </a>
+      <NavItem name="Home" icon={HomeIcon} />
+      <NavItem name="Dashboard" icon={BarChart} />
+      <NavItem name="Projects" icon={SquareStack} />
+      <NavItem name="Tasks" icon={CheckSquare} />
+      <NavItem name="Reporting" icon={Flag} />
+      <NavItem name="Users" icon={Users} />
     </nav>
   )
 }
