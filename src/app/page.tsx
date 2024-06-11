123cad7acd3dd1fb738c15fd40d15278580a8315
Maia <matteusmaia889@gmail.com>
2024-06-11 17:19:31 -0300

    faet: Home

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <div className="h-screen text-gray-300  p-4">
+      <h1 className="text-5xl before:w-1 before:h-8 before:bg-teal-500 flex items-center">
+        Home
+      </h1>
+      <p>Algo importante aqui</p>
+      <p>Algo importante aqui também</p>
+    </div>
+  )
+}
