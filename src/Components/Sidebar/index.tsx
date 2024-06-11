4f203f01243a45092e1e26994b6b1461532868ca
Maia <matteusmaia889@gmail.com>
2024-06-11 17:26:56 -0300

    update: replaced space-y-6 with flex to be able to use mt-auto

--- src/Components/Sidebar/index.tsx
+++ src/Components/Sidebar/index.tsx
@@ -4,7 +4,7 @@ import { MainNavigation } from '../MainNavigation'
 
 export function Sidebar() {
   return (
-    <aside className="border-r border-zinc-200 px-5 py-8 space-y-6">
+    <aside className="border-r border-zinc-200 px-5 py-8 flex flex-col gap-6 ">
       <Logo />
       <div className="flex w-full items-center gap-2 rounded-lg border border-zinc-300 px-3 py-2 shadow-sm ">
         <Search size={24} color="blue" className="h-5 w-5 text-zinc-500" />
@@ -15,6 +15,7 @@ export function Sidebar() {
         />
       </div>
       <MainNavigation />
+      <div className="mt-auto">kldsjmlkdsa</div>
     </aside>
   )
 }
