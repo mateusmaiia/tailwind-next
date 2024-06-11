8b55097adbf4aeb519b5c5d9fef9fc2b7e4c9c9a
Maia <matteusmaia889@gmail.com>
2024-06-11 18:29:27 -0300

    fix: problem if the gmail name is too big

--- src/Components/Sidebar/Profile.tsx
+++ src/Components/Sidebar/Profile.tsx
@@ -2,15 +2,17 @@ import { LogOut } from 'lucide-react'
 
 export function Profile() {
   return (
-    <div className="flex items-center gap-3">
+    <div className="grid grid-cols-profile gap-3">
       <img
         src="https://github.com/mateusmaiia.png"
         alt="profileImage"
         className="h-10 w-10 rounded-full"
       />
-      <div className="flex flex-col ">
+      <div className="flex flex-col truncate">
         <span className="text-sm font-semibold text-zinc-700">Mateus Maia</span>
-        <span className="text-sm  text-zinc-500">mateusssmaia@gmail.com</span>
+        <span className="text-sm  text-zinc-500 truncate">
+          mateusssmaia@gmail.com
+        </span>
       </div>
 
       <button type="button" className="ml-auto rounded-md p-2 hover:bg-zinc-50">
