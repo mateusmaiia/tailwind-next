fdbc20a48d8cc2f2bb2d0341121a7ede16b041a2
Maia <matteusmaia889@gmail.com>
2024-06-11 18:12:50 -0300

    feat/remove: import Profile and remove Search icon color

--- src/Components/Sidebar/index.tsx
+++ src/Components/Sidebar/index.tsx
@@ -12,13 +12,14 @@ import {
 import { Logo } from './Logo'
 import { NavItem } from '../MainNavigation/NavItem'
 import { UsedSpaceWidget } from './UsedSpaceWidget'
+import { Profile } from './Profile'
 
 export function Sidebar() {
   return (
     <aside className="border-r border-zinc-200 px-5 py-8 flex flex-col gap-6 ">
       <Logo />
       <div className="flex w-full items-center gap-2 rounded-lg border border-zinc-300 px-3 py-2 shadow-sm ">
-        <Search size={24} color="blue" className="h-5 w-5 text-zinc-500" />
+        <Search size={24} className="h-5 w-5 text-zinc-500" />
         <input
           className="flex-1 border-0 bg-transparent p-0 outline-none text-zinc-900 placeholder:zinc-600"
           type="text"
@@ -41,6 +42,7 @@ export function Sidebar() {
         <UsedSpaceWidget />
       </div>
       <div className="h-px bg-zinc-200"></div>
+      <Profile />
     </aside>
   )
 }
