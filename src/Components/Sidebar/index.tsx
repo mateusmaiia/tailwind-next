2b4231305969cec4d2dc91ce4b8de1ab0c713485
Maia <matteusmaia889@gmail.com>
2024-06-11 17:58:38 -0300

    feat: button user information

--- src/Components/Sidebar/index.tsx
+++ src/Components/Sidebar/index.tsx
@@ -1,8 +1,10 @@
 import {
   BarChart,
   CheckSquare,
+  Cog,
   Flag,
   HomeIcon,
+  LifeBuoy,
   Search,
   SquareStack,
   Users,
@@ -30,7 +32,40 @@ export function Sidebar() {
         <NavItem name="Reporting" icon={Flag} />
         <NavItem name="Users" icon={Users} />
       </nav>
-      <div className="mt-auto">kldsjmlkdsa</div>
+      <div className="mt-auto flex flex-col space-y-0.5">
+        <nav>
+          <NavItem name="Support" icon={LifeBuoy} />
+          <NavItem name="Setting" icon={Cog} />
+        </nav>
+        <div className="flex flex-col gap-4 rounded-lg bg-violet-50 px-4 py-4">
+          <div className="space-y-1">
+            <span className="text-sm/5 font-medium text-violet-700">
+              Used space
+            </span>
+            <p className="text-sm/5 text-violet-500">
+              Your team has used 80% of oyour available space. Need more?
+            </p>
+          </div>
+          <div className="h-2 rounded-full bg-violet-100">
+            <div className="h2 w-4/5 rounded full bg-violet-500"></div>
+          </div>
+
+          <div className="space-x-3">
+            <button
+              type="button"
+              className="text-sm font-medium text-violet-500 hover:text-violet-700"
+            >
+              Dismiss
+            </button>
+            <button
+              type="button"
+              className="text-sm font-medium text-violet-700 hover:text-violet-900"
+            >
+              Upgrade plan
+            </button>
+          </div>
+        </div>
+      </div>
     </aside>
   )
 }
